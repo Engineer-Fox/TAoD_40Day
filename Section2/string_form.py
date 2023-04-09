36d5beb366d2fc3434b0283a1fef6a1a4fbef9d9
ColoradoFox <ColoradoFox@users.noreply.github.com>
2023-04-08 20:59:17 -0600

    [#3] Continued, updated string_form

--- Section2/string_form.py
+++ Section2/string_form.py
@@ -2,8 +2,11 @@ name = 'Dalton'
 age = 32
 money = 1.1
 
+#print using string concatenation
 print(name + ' is ' + str(age) + ' and has $' + str(money) + ' million, in the bank!')
 
+#print using the .format()
 print("{0} is {1} and has ${2} million in the bank!".format(name, age, money))
 
+# print using f-strings
 print(f'{name} is {age} and has ${money} million in the bank!')
