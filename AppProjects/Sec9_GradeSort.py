c25351dd5f40b7233265669972c67bf93d22bb36
Fox-Dalton <100093992+Fox-Dalton@users.noreply.github.com>
2023-04-16 08:47:06 -0600

    [16042023] Completed Sec9

--- AppProjects/Sec9_GradeSort.py
+++ AppProjects/Sec9_GradeSort.py
@@ -1,1 +1,31 @@
 print('Welcome to the grade sorter app!')
+
+# user_grade1 = input('What is your first grade? (0-100): ')
+# user_grade2 = input('What is your second grade? (0-100): ')
+# user_grade3 = input('What is your third grade? (0-100): ')
+# user_grade4 = input('What is your fourth grade? (0-100): ')
+
+user_grades = []
+
+grade = int(input('What is your first grade? (0-100): '))
+user_grades.append(grade)
+grade = int(input('What is your second grade? (0-100): '))
+user_grades.append(grade)
+grade = int(input('What is your third grade? (0-100): '))
+user_grades.append(grade)
+grade = int(input('What is your fourth grade? (0-100): '))
+user_grades.append(grade)
+
+print('\n Your grades are: ' + str(user_grades))
+
+user_grades.sort(reverse=True)
+print('\n Your grades from highest to lowest are: ' + str(user_grades))
+
+print('\n The lowest two grades will now be dropped.')
+removed_grade = user_grades.pop()
+print('Removed grade: ' + str(removed_grade))
+removed_grade = user_grades.pop()
+print('Removed grade: ' + str(removed_grade))
+
+print('Your remaining grades are: ' + str(user_grades))
+print('Nice work!  Your highest grade is: ' + str(user_grades[0]) + ' .')
