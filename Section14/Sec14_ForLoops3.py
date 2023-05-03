758235eae26b6188d9b69814561962c7c44126a1
Dalton Fox <fox.engineer@outlook.com>
2023-05-03 10:28:54 -0600

    [03052023] Sec14_ForLoops3 and 4 changes

--- Section14/Sec14_ForLoops3.py
+++ Section14/Sec14_ForLoops3.py
@@ -5,7 +5,21 @@ for num in numbers:
     print(num)
 
 print('\n slicing!')
-for num in numbers[0:5]:
+# for num in numbers[0:5]:
+for num in numbers[5:]:
     print(num)
 
+new_numbers = numbers
+print(new_numbers)
+print(type(new_numbers))
 #left here on 02.05.2023
+
+# numbers.pop()
+# print(numbers)
+# print(new_numbers)
+
+new_numbers = numbers[:]
+print(new_numbers)
+numbers.pop()
+print(numbers)
+print(new_numbers)
---
+++ Section14/Sec14_ForLoops4.py
@@ -0,0 +1,9 @@
+#using zip() function
+
+names = ["jack", "john", "mark", "paul"]
+numbers = [20,44,3,15]
+for name in names:
+    print(name)
+
+for i in range(len(names)):
+    print(names[i]+': '+str(numbers[i]))
