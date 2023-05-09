b02da5d4b74010c1dfe10cafab52ee91d8772637
Dalton Fox <fox.engineer@outlook.com>
2023-05-09 16:09:35 -0600

    [05092023]

---
+++ AppProjects/Sec15_BinaryHex.py
@@ -0,0 +1,4 @@
+print('Welcome to the Binary/Hexidecimal Converter App!')
+
+max_value = int(input('Compute binary and hexidecimal values up to the following decimal number: '))
+decimal = list(range(1,max_value+1))
