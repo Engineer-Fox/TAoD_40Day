22d7a4a6d0d160028317efbf2bbfa979b34857b3
Dalton Fox <fox.engineer@outlook.com>
2023-07-31 15:29:26 -0600

    [07312023] Sec24 Additional Updates

--- Section24/Sec24_Dictionary.py
+++ Section24/Sec24_Dictionary.py
@@ -17,4 +17,12 @@ bob_info['height'] = 76.5
 
 print(bob_info)
 bob_info['weight'] -=6
-print(f"Wow {bob_info['first_name']}, you lost some weight! You now weight {bob_info['weight']}!")
+print(f"Wow {bob_info['first_name']}, you lost some weight! You now weight {bob_info['weight']} pounds!")
+bob_info['age'] += 1
+print(f'Happy birthday {bob_info["first_name"]}! You are now {bob_info["age"]}!')
+
+user_info = {}
+user_info['name'] = input('What is your name:  ').title()
+user_info['age'] = input('What is your age: ').title()
+user_info['job'] = input('What is your job:  ').title()
+print(user_info)
