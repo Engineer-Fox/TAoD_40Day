58244bf4aae320572570a82ba13c00e6e9b401c9
Fox-Dalton <100093992+Fox-Dalton@users.noreply.github.com>
2023-08-02 13:47:00 -0600

    [08022023] Sec24 Updates

---
+++ Section24/Sec24_Dictionaries1.py
@@ -0,0 +1,15 @@
+# First line test
+
+bob_info ={
+    "first_name":"Bob",
+    "last_name":"Fox",
+    "middle_name":"Michael",
+    "age":32,
+    "fav_colors":['green','orange'],
+}
+
+print(bob_info)
+print(type(bob_info))
+
+print(bob_info['age'])
+print(bob_info['fav_colors'][0])
