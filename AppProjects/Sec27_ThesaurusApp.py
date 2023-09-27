3fe30df749a1998531cab7b8ca9759c4424b3723
Dalton Fox <fox.engineer@outlook.com>
2023-09-27 14:24:43 -0600

    Complete Section27, used other code not mine

--- AppProjects/Sec27_ThesaurusApp.py
+++ AppProjects/Sec27_ThesaurusApp.py
@@ -1,26 +1,42 @@
-#Thesaurus app
 import random
-print('Welcome to the thesaurus app!')
-
+ 
+#define thesauraus
 thesaurus = {
-        'happy':['marry','gay','extatic','elated','jovial'],
-        'hot':['scorching','sweaty','stifling','boiling','tropical'],
-        'cold':['frigid','freezing','cool','polar','chilly'],
-        'sad':['morose','unhappy','downtrodden','miserable','glum'],
+    "hot":['balmy', 'summery', 'tropical', 'boiling', 'scorching'],
+    "cold":['chilly', 'cool', 'freezing', 'frigid', 'polar'],
+    "happy":['content', 'cheery', 'merry', 'jovial', 'jocular'],
+    "sad":['unhappy', 'downcast', 'miserable', 'glum', 'melancholy'],
+ 
 }
-
-print('Choose a word from the thesaurus and I will give you a synonym!')
-print('Here are the words in the thesaurus:')
+ 
+#messages
+print("Welcome to the Thesaurus App!")
+print("\nChoose a word from the thesaurus and I will give you a synonym.")
+print("\nHere are the words in the thesaurus:")
+ 
+#output keyd
 for key in thesaurus.keys():
-    print(f'\t-{key}')
-# get user info
-choice = input('\nWhat word would you like a synonym for?: ').lower().strip
-#provide random synonym
+    print(f"\t-{key}")
+ 
+# get selection and display random value
+choice = input ("What word would you like a synonym for: ").strip().lower()
 if choice in thesaurus.keys():
-    index = random.randint(0,4)
-    print(f'A synonym for {choice} is {str(thesaurus[choice])}.')
-#Choose a word from the thesaurus and I will give you a synonym
-#Here are the words in the thes
-#What word would you like a syn for?
-#A syn for ___ is merry
-#Would you like to see the whole thes? (yes/no):
+    words = thesaurus[choice]
+    # This was the issue with my code, it seems like the range was incorrect
+    index = random.randint(0, len(words)-1)
+    print("A synonym for " + choice + " is " + words[index] + ".")
+ 
+# print error if choice is not in the dictionary
+else:
+    print("not there")
+ 
+# ask if they want to display the thesaurus
+answer = input("\nWould you like to see the whole thesaurus (yes/no): ").lower().strip()
+if answer.startswith("y"):
+    for key, values in thesaurus.items():
+        print("\n" + key +" synonyms are:")
+        for value in values:
+            print("\t-" + value)
+ 
+else:
+    print("I hope you enjoyed the program. Thank you!")
