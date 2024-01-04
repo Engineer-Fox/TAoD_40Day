69d1bed8c3525c73e87bafba6179f5f97c0023c5
DaltonFox <fox.engineer@outlook.com>
2024-01-04 08:55:19 -0700

    [20240104] Sec42, Complete; Sec43, In Porgress

--- AppProjects/Sec42_TicTacToeApp.py
+++ AppProjects/Sec42_TicTacToeApp.py
@@ -1,26 +1,79 @@
 '''Tic Tac Toe App'''
 
-def draw_board(charlist):
+def draw_board(char_list):
     '''Print a game board'''
     print('\n\t\tTicTacToe')
     print('\t~~~~~~~~~~~~~~~~~~~~~~~')
-    print('\t|| ', charlist[0],' || ',charlist[1],' || ',charlist[2],' ||')
+    print('\t|| ', char_list[0],' || ',char_list[1],' || ',char_list[2],' ||')
     print('\t-----------------------')
-    print('\t|| ', charlist[3],' || ',charlist[4],' || ',charlist[5],' ||')
+    print('\t|| ', char_list[3],' || ',char_list[4],' || ',char_list[5],' ||')
     print('\t-----------------------')
-    print('\t|| ', charlist[6],' || ',charlist[7],' || ',charlist[8],' ||')
+    print('\t|| ', char_list[6],' || ',char_list[7],' || ',char_list[8],' ||')
 
 def get_player_in(player_char, char_list):
     '''Get player info'''
+    while True:
+        player_move = int(input(player_char + ': Where would you like to place your piece? (1-9): '))
+        if player_move > 0 and player_move <10:
+            if char_list[player_move-1] == '_':
+                return player_move
+            else:
+                print('That spot has already been taken.  Try again.')
+        else:
+            print('That is not a good spot on the board.  Please try again.')
 
-def place_char_board():
+
+def place_char_board(player_char, player_move, char_list):
     '''Place character on board'''
-def is_winner():
-    '''Determine winner of game'''
-player1 = 'X'
-player2 = 'O'
-nlist = ['1','2','3','4','5','6','7','8','9']
-clist = ['-']*9
+    char_list[player_move-1] = player_char
 
-draw_board(clist)
-draw_board(nlist)
+def is_winner(pC, cL):
+    '''Determine winner of game'''
+    return((cL[0]== pC and cL[1] == pC and cL[2] == pC) or # Rows
+           (cL[3]== pC and cL[4] == pC and cL[5] == pC) or
+           (cL[6]== pC and cL[7] == pC and cL[8] == pC) or 
+           (cL[0]== pC and cL[3] == pC and cL[6] == pC) or # Columns
+           (cL[1]== pC and cL[4] == pC and cL[7] == pC) or
+           (cL[2]== pC and cL[5] == pC and cL[8] == pC) or
+           (cL[0]== pC and cL[4] == pC and cL[8] == pC) or # Diagonals
+           (cL[2]== pC and cL[4] == pC and cL[6] == pC))
+
+
+player_1 = 'X'
+player_2 = 'O'
+n_list = ['1','2','3','4','5','6','7','8','9']
+c_list = ['_']*9
+
+# draw_board(c_list)
+# draw_board(n_list)
+
+# move = get_player_in(player_1, c_list)
+# place_char_board(player_1,move, c_list)
+# draw_board(c_list)
+# draw_board(n_list)
+
+draw_board(n_list)
+draw_board(c_list)
+
+while True:
+    # Player 1's Turn
+    move = get_player_in(player_1, c_list)
+    place_char_board(player_1,move,c_list)
+    # Re draw game board
+    draw_board(n_list)
+    draw_board(c_list)
+    # check to see if player 1 won
+    if is_winner(player_1,c_list):
+        print('Player 1, WINS!!!')
+        break
+    elif "_" not in c_list:
+        print('The game was a tie.')
+    
+    move = get_player_in(player_2, c_list)
+    place_char_board(player_2,move,c_list)
+    draw_board(n_list)
+    draw_board(c_list)
+    if is_winner(player_2,c_list):
+        print('Player 2, WINS!!!')
+        break
+
--- AppProjects/Sec43_LoanCalcApp.py
+++ AppProjects/Sec43_LoanCalcApp.py
@@ -1,1 +1,21 @@
 '''Mortgage Loan App'''
+from mortgage import Loan
+from matplotlib import pyplot
+loan = Loan(principal=380000,interest=0.0267,term=30)
+
+totalpaid = loan.total_paid
+totalinterest = loan.total_interest
+totalprincipal = loan.total_principal
+# pyplot.plot(34,70)
+print(totalpaid)
+print(totalinterest)
+print(totalprincipal)
+def plot_loan(value,**kwargs):
+    pyplot.yscale(str(totalpaid),**kwargs=linear)
+    pyplot.xscale(str(loan.term), **kwargs=linear)
+    pyplot.show
+def create_graph(data, loan):
+    x_values = []
+    y_values = []
+
+    
