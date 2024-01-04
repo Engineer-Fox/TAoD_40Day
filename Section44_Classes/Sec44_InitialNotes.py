0513f5915b94b1e57fff4c222438f6103b05de7a
DaltonFox <fox.engineer@outlook.com>
2024-01-04 12:36:52 -0700

    [20240104] Sec44, In Progress

--- Section44_Classes/Sec44_InitialNotes.py
+++ Section44_Classes/Sec44_InitialNotes.py
@@ -1,25 +1,55 @@
 #  Creating a class
-
+import random
 class Baby():
     '''A simple class to simulate a baby'''
     #  Init method for the class Baby
     def __init__(self,name,sex,age):
         # Initialize attributes
         self.name = name.title()
-        self.sex = sex
+        self.sex = sex.lower()
         self.age = age
 
         self.tired = True
     
     def play(self):
-        pass
+        if self.sex == 'male':
+            print(self.name + ' is playing with cars.')
+        else:
+            print(self.name + ' is playing with blocks.')
 
     def cry(self):
-        pass
-
+        '''Simulate crying'''
+        print('WAAAH WAAAH WAAAH')
+        print('even ' + str(self.age)+ ' year olds cry.')
+        
     def sleep(self):
-        pass
+        if self.tired:
+            print(self.name + ' is sleeping... FINALLY!')
+            self.tired = False
+
+        else:
+            print('Sorry! ' + self.name + ' is not tired...')
 
 little_boy = Baby('John','male',3)
 little_girl = Baby('mary','female',5)
-print(little_boy.name)
+print(little_boy.name+' is a '+str(little_boy.age)+' year old '+little_boy.sex+'.')
+print(little_girl.name+' is a '+str(little_girl.age)+' year old '+little_girl.sex+'.')
+little_boy.play()
+little_girl.play()
+little_boy.sleep()
+little_girl.sleep()
+little_boy.sleep()
+
+babies = []
+for i in range(1,101,1):
+    num = random.randint(0,1)
+    if num == 0:
+        sex = 'male'
+    else:
+        sex = 'female'
+    age = random.randint(0,5)
+    baby = Baby(str(i),sex, age)
+    babies.append(baby)
+
+for baby in babies:
+    print(baby.name + ' is a '+str(baby.age)+' year old '+ baby.sex+'.')
